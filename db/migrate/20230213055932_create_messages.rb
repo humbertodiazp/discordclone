2d7289272ed80df36267554bb085c5792497351d
Humberto <hdkaratechop@gmail.com>
2023-02-16 20:55:34 -0800

    remove text to allow migration

--- db/migrate/20230213055932_create_messages.rb
+++ db/migrate/20230213055932_create_messages.rb
@@ -1,7 +1,6 @@
 class CreateMessages < ActiveRecord::Migration[7.0]
   def change
     create_table :messages do |t|
-      t.string :tex.string
       t.string :email
       t.string :user_id
       t.string :second_user_id
