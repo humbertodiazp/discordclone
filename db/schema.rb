558c434f7249446c1ff8d9adf10af56e64b25c9e
Humberto <hdkaratechop@gmail.com>
2023-02-16 21:30:28 -0800

    run migrations to udpate schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_17_044920) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_17_052856) do
   create_table "messages", force: :cascade do |t|
     t.string "email"
     t.string "user_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_17_044920) do
     t.string "second_user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user"
   end
 
   create_table "users", force: :cascade do |t|
