caf87ecd5750a5e1660d63fadfe5b1b941129324
Humberto <hdkaratechop@gmail.com>
2023-02-16 21:52:34 -0800

    run migrations and update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_17_052856) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_17_055120) do
   create_table "messages", force: :cascade do |t|
     t.string "email"
     t.string "user_id"
@@ -22,11 +22,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_17_052856) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "name"
-    t.string "creator_id"
     t.string "second_user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "user"
+    t.string "user_id"
   end
 
   create_table "users", force: :cascade do |t|
